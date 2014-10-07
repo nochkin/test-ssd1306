122353b9ba076bf3eb6acd148da89b58800d5c27
root <root@Raspyfi>
2014-10-07 22:26:15 +0100

    Monitor class related changes

--- Makefile
+++ Makefile
@@ -10,9 +10,9 @@ FIND = find
 OBJDIR = .obj
 
 SRC = bcm2835.c glcdfont.c
-SRCXX = main.cpp Adafruit_GFX.cpp Adafruit_SSD1306.cpp mpclient.cpp
+SRCXX = main.cpp Adafruit_GFX.cpp Adafruit_SSD1306.cpp mpclient.cpp monitor.cpp
 INC = bcm2835.h
-INCXX = SSD1306.h Adafruit_GFX.h Adafruit_SSD1306.h mpclient.h
+INCXX = SSD1306.h Adafruit_GFX.h Adafruit_SSD1306.h mpclient.h monitor.h
 OBJ = $(patsubst %,$(OBJDIR)/%,$(SRC:.c=.c.o))
 OBJXX = $(patsubst %,$(OBJDIR)/%,$(SRCXX:.cpp=.cpp.o))
 BINARY = ssd1306-mpc
--- main.cpp
+++ main.cpp
@@ -2,64 +2,27 @@
 
 #include <mpd/status.h>
 
-#include "mpclient.h"
-#include "Adafruit_SSD1306.h"
-#include "Adafruit_GFX.h"
+#include "monitor.h"
 
 int main(int argc, char *argv[])
 {
-	Adafruit_SSD1306 display;
-	MPClient mpc_client;
+	int err;
 
-	if (display.init_i2c() != 0) {
+	Monitor mon;
+
+	err = mon.setup_display();
+	if (err != 0) {
 		printf("Failed init OLED\n");
-		return 2;
+		return err;
 	}
 
-	display.clearDisplay();
-
-	display.setTextSize(1);
-	display.setTextColor(WHITE);
-	display.setCursor(0,0);
-	display.write('t');
-    display.drawCircle(3, 3, 8, WHITE);
-	display.display();
-
-	mpc_client.connect();
-	mpc_client.loop();
-
-	mpc_client.update_status();
-	printf("mpc: volume: %i\n", mpc_client.get_info_volume());
-	printf("mpc: state: ");
-	switch (mpc_client.get_info_state()) {
-		case MPD_STATE_STOP:
-			printf("stop\n");
-			break;
-		case MPD_STATE_PLAY:
-			printf("play\n");
-			break;
-		case MPD_STATE_PAUSE:
-			printf("pause\n");
-			break;
+	err = mon.setup_mpc();
+	if (err != 0) {
+		printf("Failed init mpc\n");
+		return err;
 	}
-	printf("mpc: time: %i\n", mpc_client.get_info_elapsed_time());
-	printf("mpc: audio: %iHz/%ibit", mpc_client.get_info_sample_rate(), mpc_client.get_info_bits());
-	switch(mpc_client.get_info_channels()) {
-		case 0:
-			printf("\n");
-			break;
-		case 1:
-			printf("/mono\n");
-			break;
-		case 2:
-			printf("/stereo\n");
-			break;
-		default:
-			printf("/multi-channel\n");
-			break;
-	}
-	printf("mpc: title: %s\n", mpc_client.get_info_title().c_str());
-	printf("mpc: album: %s\n", mpc_client.get_info_album().c_str());
+
+	mon.watch_loop();
 
 	return 0;
 }
